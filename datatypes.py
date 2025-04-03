691e1dcf64489c7d27f3f1ad58ec015fae8c89e1
theonebaahir <babusadudekula@gmail.com>
2025-04-03 21:25:17 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+# data types
+a = 5
+print("type of a" , type(a))
+b = 5.5
+print("type of b" , type(b))
+c = "coding"
+print("type of c" , type(c))
+d = True
+print("type of d" , type(d))
+# typecasting means converting one data type to another
+weight = 30.4596543213
+w = int(weight)
+print(w)
+a = float(a)
+print(a)
+
+b =30
+b = str(30)
+z = 23
+y = 567
+print(z+y)
+# reverse a string 
+text = str(input("enter a string:"))
+revText = text[::-1]
+text = revText
+print("Reverse of given string is:")
+print(text)
